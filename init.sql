cbda28cb72e07bb349e2da615327a876c836fd14
reflektoin <66482139+reflektoin@users.noreply.github.com>
2022-10-11 22:39:12 +0300

    Update init.sql
    Move \COPY commands to  single lines. Previous version threw an error because of newlines.

--- init.sql
+++ init.sql
@@ -16,105 +16,16 @@ CREATE TABLE hankinta (
         hankintakategoria VARCHAR NOT NULL
 );
 
-\COPY hankinta (
-lasku_id,
-tili,
-tiliointisumma,
-tositepvm,
-toimittaja_y_tunnus,t
-oimittaja_nimi,
-hankintayksikko_tunnus,
-hankintayksikko,
-ylaorganisaatio_tunnus,
-ylaorganisaatio,
-sektori,
-tuote_palveluryhma,
-hankintakategoria) FROM './th_data_2022.csv' DELIMITER E'\t' CSV HEADER;
+\COPY hankinta (lasku_id,tili,tiliointisumma,tositepvm,toimittaja_y_tunnus,toimittaja_nimi,hankintayksikko_tunnus,hankintayksikko,ylaorganisaatio_tunnus,ylaorganisaatio,sektori,tuote_palveluryhma,hankintakategoria) FROM './th_data_2022.csv' DELIMITER E'\t' CSV HEADER;
 
-\COPY hankinta (
-lasku_id,
-tili,
-tiliointisumma,
-tositepvm,
-toimittaja_y_tunnus,
-toimittaja_nimi,
-hankintayksikko_tunnus,
-hankintayksikko,
-ylaorganisaatio_tunnus,
-ylaorganisaatio,
-sektori,
-tuote_palveluryhma,
-hankintakategoria) FROM './th_data_2021.csv' DELIMITER E'\t' CSV HEADER;
-\COPY hankinta (
-lasku_id,
-tili,
-tiliointisumma,
-tositepvm,
-toimittaja_y_tunnus,
-toimittaja_nimi,
-hankintayksikko_tunnus,
-hankintayksikko,
-ylaorganisaatio_tunnus,y
-laorganisaatio,
-sektori,
-tuote_palveluryhma,
-hankintakategoria) FROM './th_data_2020.csv' DELIMITER E'\t' CSV HEADER;
+\COPY hankinta (lasku_id,tili,tiliointisumma,tositepvm,toimittaja_y_tunnus,toimittaja_nimi,hankintayksikko_tunnus,hankintayksikko,ylaorganisaatio_tunnus,ylaorganisaatio,sektori,tuote_palveluryhma,hankintakategoria) FROM './th_data_2021.csv' DELIMITER E'\t' CSV HEADER;
 
-\COPY hankinta (
-lasku_id,
-tili,
-tiliointisumma,
-tositepvm,
-toimittaja_y_tunnus,
-toimittaja_nimi,
-hankintayksikko_tunnus,
-hankintayksikko,
-ylaorganisaatio_tunnus,
-ylaorganisaatio,
-sektori,
-tuote_palveluryhma,
-hankintakategoria) FROM './th_data_2019.csv' DELIMITER E'\t' CSV HEADER;
+\COPY hankinta (lasku_id,tili,tiliointisumma,tositepvm,toimittaja_y_tunnus,toimittaja_nimi,hankintayksikko_tunnus,hankintayksikko,ylaorganisaatio_tunnus,ylaorganisaatio,sektori,tuote_palveluryhma,hankintakategoria) FROM './th_data_2020.csv' DELIMITER E'\t' CSV HEADER;
 
-\COPY hankinta (lasku_id,
-tili,
-tiliointisumma,
-tositepvm,
-toimittaja_y_tunnus,
-toimittaja_nimi,
-hankintayksikko_tunnus,
-hankintayksikko,
-ylaorganisaatio_tunnus,
-ylaorganisaatio,
-sektori,
-tuote_palveluryhma,
-hankintakategoria) FROM './th_data_2018.csv' DELIMITER E'\t' CSV HEADER;
+\COPY hankinta (lasku_id,tili,tiliointisumma,tositepvm,toimittaja_y_tunnus,toimittaja_nimi,hankintayksikko_tunnus,hankintayksikko,ylaorganisaatio_tunnus,ylaorganisaatio,sektori,tuote_palveluryhma,hankintakategoria) FROM './th_data_2019.csv' DELIMITER E'\t' CSV HEADER;
 
-\COPY hankinta (
-lasku_id,
-tili,
-tiliointisumma,
-tositepvm,
-toimittaja_y_tunnus,
-toimittaja_nimi,
-hankintayksikko_tunnus,
-hankintayksikko,
-ylaorganisaatio_tunnus,
-ylaorganisaatio,
-sektori,
-tuote_palveluryhma,
-hankintakategoria) FROM './th_data_2017.csv' DELIMITER E'\t' CSV HEADER;
+\COPY hankinta (lasku_id,tili,tiliointisumma,tositepvm,toimittaja_y_tunnus,toimittaja_nimi,hankintayksikko_tunnus,hankintayksikko,ylaorganisaatio_tunnus,ylaorganisaatio,sektori,tuote_palveluryhma,hankintakategoria) FROM './th_data_2018.csv' DELIMITER E'\t' CSV HEADER;
 
-\COPY hankinta (
-lasku_id,
-tili,
-tiliointisumma,
-tositepvm,
-toimittaja_y_tunnus,
-toimittaja_nimi,
-hankintayksikko_tunnus,
-hankintayksikko,
-ylaorganisaatio_tunnus,
-ylaorganisaatio,
-sektori,
-tuote_palveluryhma,
-hankintakategoria) FROM './th_data_2016.csv' DELIMITER E'\t' CSV HEADER;
+\COPY hankinta (lasku_id,tili,tiliointisumma,tositepvm,toimittaja_y_tunnus,toimittaja_nimi,hankintayksikko_tunnus,hankintayksikko,ylaorganisaatio_tunnus,ylaorganisaatio,sektori,tuote_palveluryhma,hankintakategoria) FROM './th_data_2017.csv' DELIMITER E'\t' CSV HEADER;
+
+\COPY hankinta (lasku_id,tili,tiliointisumma,tositepvm,toimittaja_y_tunnus,toimittaja_nimi,hankintayksikko_tunnus,hankintayksikko,ylaorganisaatio_tunnus,ylaorganisaatio,sektori,tuote_palveluryhma,hankintakategoria) FROM './th_data_2016.csv' DELIMITER E'\t' CSV HEADER;
